710cc57d162233da3566750626b2b719adf8451e
Ezequiel Barbero <ebarbero@asapp.com>
2024-08-12 12:20:57 -0300

    Fixes telescope

--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,12 @@
-local builtin = require('telescope.builtin')
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     -- config = true,
     keys = {
-        { '<leader>ff', builtin.find_files },
-        { '<leader>fg', builtin.live_grep },
-        { '<leader>fb', builtin.buffers },
-        { '<leader>fh', builtin.help_tags },
+        { '<leader>ff', function() require('telescope.builtin').find_files() end },
+        { '<leader>fg', function() require('telescope.builtin').live_grep() end },
+        { '<leader>fb', function() require('telescope.builtin').buffers() end },
+        { '<leader>fh', function() require('telescope.builtin').help_tags() end },
     }
 }
 -- nvim-treesitter/nvim-treesitter (finder/preview)
