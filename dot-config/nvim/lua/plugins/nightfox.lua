9f49d06ba016a75da43fd03d0d8872f0806a0914
Ezequiel Barbero <ebarbero@asapp.com>
2025-02-21 12:15:49 -0300

    Dark theme

--- dot-config/nvim/lua/plugins/nightfox.lua
+++ dot-config/nvim/lua/plugins/nightfox.lua
@@ -4,11 +4,11 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         -- vim.cmd("colorscheme nightfox")
-        vim.cmd("colorscheme dayfox")
+        -- vim.cmd("colorscheme dayfox")
         -- vim.cmd("colorscheme dawnfox")
         -- vim.cmd("colorscheme duskfox")
         -- vim.cmd("colorscheme nordfox")
         -- vim.cmd("colorscheme terafox")
-        -- vim.cmd("colorscheme carbonfox")
+        vim.cmd("colorscheme carbonfox")
     end,
 }
